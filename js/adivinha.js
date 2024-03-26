7e31f839de01dd9fb92c26d85acabaea6c451c80
José Leandro Vilela <joseleandro2807@gmail.com>
2024-03-26 14:31:02 -0300

    att

--- js/adivinha.js
+++ js/adivinha.js
@@ -5,9 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
    const contadorH3 = document.getElementById('contador');
    const resultado = document.getElementById('jogadas');
    let contador = 0;
-   contadorH3.textContent = 'Contador de Jogadas: ' + contador;
+   
    resultado.textContent = 'Para iniciar digite um número e clique em palpites';
-    const play = document.getElementById('play');
+   const play = document.getElementById('play');
+
+    
+
 
     play.addEventListener('click', function(){
         let sumir = document.getElementById('sumir');
@@ -23,22 +26,53 @@ document.addEventListener('DOMContentLoaded', function() {
    botaoReiniciar.addEventListener('click', reiniciar);
     
 
-    function verificarAcerto(){
-        contador++;
-        contadorH3.textContent = 'Contador de Jogadas: ' + contador;
-        const inputPalpite = document.getElementById('palpites').value;
+   function verificarPalpite(inputPalpite, selecioneDificuldade) {
+    if (inputPalpite == numero && contador > 0 && contador <= getLimiteJogadas(selecioneDificuldade)) {
+        resultado.textContent = 'Parabéns você acertou o número!';
+    } else if (inputPalpite < numero && contador > 0 && contador <= getLimiteJogadas(selecioneDificuldade)) {
+        resultado.textContent = 'O número é maior que ' + inputPalpite;
+    } else if (inputPalpite > numero && contador > 0 && contador <= getLimiteJogadas(selecioneDificuldade)) {
+        resultado.textContent = 'O número é menor que ' + inputPalpite;
+    } else if (contador == 0) {
+        contadorZero();
+    }
+}
 
-        if(inputPalpite == numero && contador < 8){
-            resultado.textContent = 'Parabéns você acertou o número!';
-        }else if(inputPalpite < numero && contador < 8){
-            resultado.textContent = 'O número é maior que ' + inputPalpite;
-        }else if(inputPalpite > numero && contador < 8){
-            resultado.textContent = 'O número é menor que ' + inputPalpite;
-        }else if(contador >= 8){
-            resultado.textContent = 'Suas tentativas acabaram, o número era ' + numero;
-            contadorH3.textContent = 'Clique em Reiniciar para recomeçar';
-        }
+function getLimiteJogadas(dificuldade) {
+    switch (dificuldade) {
+        case 'facil':
+            return 15;
+        case 'medio':
+            return 10;
+        case 'dificil':
+            return 5;
+        default:
+            return 0;
+    }
+}
 
+function verificarAcerto() {
+    const inputPalpite = document.getElementById('palpites').value;
+    const selecioneDificuldade = document.getElementById('dificuldade').value;
+
+    if (contador == 0) {
+        contador = getLimiteJogadas(selecioneDificuldade);
+        contadorH3.textContent = 'Jogadas Restantes: ' + contador;
+    }
+
+    contador--;
+    contadorH3.textContent = 'Jogadas Restantes: ' + contador;
+
+    verificarPalpite(inputPalpite, selecioneDificuldade);
+}
+
+
+
+    function contadorZero(){
+                resultado.textContent = 'Suas tentativas acabaram, o número era ' + numero;
+                contadorH3.textContent = 'Clique em Reiniciar para recomeçar';
+                document.getElementById('palpites').value = '';
+                botaoPalpite.disabled = true;
     }
 
 
@@ -47,7 +81,9 @@ document.addEventListener('DOMContentLoaded', function() {
         contador = 0;
         contadorH3.textContent = 'Contador de Jogadas: ' + contador;
         resultado.textContent = 'Para iniciar digite um número e clique em palpites';
-        document.getElementById('palpites').value = '';
+        
+        botaoPalpite.disabled = false;
+        
     }
 
 
